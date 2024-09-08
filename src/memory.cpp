354c4a2ba5f831fcc745b31204327f607afa9576
Toasterbirb <git@toasterbirb.com>
2024-09-08 16:09:11 +0300

    Attempt to optimize malloc slightly
    Its gonna need a rewrite ngl

--- src/memory.cpp
+++ src/memory.cpp
@@ -20,6 +20,9 @@ struct block
 	bool used{false};
 };
 
+constexpr u64 minimum_allocation{1024};
+static_assert(sizeof(block) + sizeof(f128) < minimum_allocation);
+
 static block blocks {nullptr, nullptr, 0, true};
 
 // keep track of allocated blocks to spot memory leakage
@@ -27,9 +30,23 @@ static mu64 block_allocation_counter{0};
 
 namespace brb
 {
+	void split_block(block* b, u64 needed_size);
+
 	mu64 allocated_block_count()
 	{
 		return block_allocation_counter;
+	}
+
+	void split_block(block* b, u64 needed_size)
+	{
+		block* next_block = static_cast<block*>(b->addr) + needed_size + 1;
+		b->next = next_block;
+		b->size = sizeof(block) + needed_size;
+
+		next_block->addr = next_block + sizeof(block);
+		next_block->next = nullptr;
+		next_block->size = b->size - needed_size - 1;
+		next_block->used = false;
 	}
 
 	void* malloc(u64 size)
@@ -40,12 +57,13 @@ namespace brb
 		while ((b->used || b->size < size) && b->next != nullptr)
 			b = b->next;
 
-		// if no fitting blocks were found, allocate a new one and add it to the chain
+		// if no fitting blocks were found, allocate new blocks and add them to the chain
 
 		if (b->used)
 		{
-			u64 block_size = size + sizeof(block);
-			block* new_block = static_cast<block*>(brb::syscall::mmap(0, block_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+			u64 needed_block_size = size + sizeof(block);
+			u64 new_block_size = size < minimum_allocation ? minimum_allocation : size + sizeof(block);
+			block* new_block = static_cast<block*>(brb::syscall::mmap(0, new_block_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
 			brb::assert(new_block != nullptr, "got a nullptr from mmap");
 
 			// create a link between the last block and the new block
@@ -55,13 +73,24 @@ namespace brb
 			new_block->addr = new_block + sizeof(block);
 
 			// set the rest of the variables
-			new_block->size = block_size;
+			new_block->size = new_block_size;
 			new_block->used = true;
 			new_block->next = nullptr;
 
 			// move into the new block
 			b = new_block;
+
+			// create a new unused block after this current one if there's more space left that's unused
+			if (needed_block_size < new_block_size)
+				split_block(new_block, needed_block_size);
 		}
+
+		// split the block if possible
+		if (b->size > size + sizeof(block))
+			split_block(b, size + sizeof(block));
+
+		// mark the block as used
+		b->used = true;
 
 		brb::assert(b->size != 0, "empty memory block");
 		brb::assert(b->used, "new memory block left unused");
